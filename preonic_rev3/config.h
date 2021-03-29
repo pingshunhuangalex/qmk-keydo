a5bf2d7157866ab36d35a6009bd07390cd979f7a
Pingshun Huang <alex.g.huang@gmail.com>
2021-03-29 18:44:48 +1100

    Minimise firmware bundle size
    Signed-off-by: Pingshun Huang <alex.g.huang@gmail.com>

--- preonic_rev3/config.h
+++ preonic_rev3/config.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#define NO_DEBUG             // Disable debugging
+#define NO_PRINT             // Disable printing/debugging using hid_listen
+
 #define FORCE_NKRO           // NKRO by default requires to be turned on, this forces it on during keyboard startup regardless of EEPROM setting. NKRO can still be turned off but will be turned on again if the keyboard reboots.
 #define TAP_CODE_DELAY 10    // Sets the delay between register_code and unregister_code, if you're having issues with it registering properly (common on VUSB boards). The value is in milliseconds.
 

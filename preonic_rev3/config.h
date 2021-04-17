db11f3448b19b94d940f4792899df8e07914f079
Pingshun Huang <alex.g.huang@gmail.com>
2021-04-18 01:45:07 +1000

    Tidy up comments and folder structure
    Signed-off-by: Pingshun Huang <alex.g.huang@gmail.com>

--- preonic_rev3/config.h
+++ preonic_rev3/config.h
@@ -1,10 +1,26 @@
+/* Copyright 2021 Pingshun Huang (Alex) <alex.g.huang@gmail.com> @<pingshunhuangalex>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
-// #define NO_DEBUG             // Disable debugging
-// #define NO_PRINT             // Disable printing/debugging using hid_listen
+// #define NO_DEBUG           // Disable debugging
+// #define NO_PRINT           // Disable printing/debugging using hid_listen
 
-#define FORCE_NKRO           // NKRO by default requires to be turned on, this forces it on during keyboard startup regardless of EEPROM setting. NKRO can still be turned off but will be turned on again if the keyboard reboots.
-#define TAP_CODE_DELAY 10    // Sets the delay between register_code and unregister_code, if you're having issues with it registering properly (common on VUSB boards). The value is in milliseconds.
+#define FORCE_NKRO         // NKRO by default requires to be turned on, this forces it on during keyboard startup regardless of EEPROM setting. NKRO can still be turned off but will be turned on again if the keyboard reboots.
+#define TAP_CODE_DELAY 10  // Sets the delay between register_code and unregister_code, if you're having issues with it registering properly (common on VUSB boards). The value is in milliseconds.
 
 #ifdef AUDIO_ENABLE
     // #define STARTUP_SONG SONG(PREONIC_SOUND)
---
+++ preonic_rev3/sphs.c
@@ -0,0 +1,52 @@
+enum preonic_layers {
+  _BASE = 0,
+  _NUM,
+  _SYM,
+  _NAV,
+  _FN
+};
+
+enum custom_keycodes {
+  DEL_HOM = SAFE_RANGE,
+  DEL_END
+};
+
+#define SET_BAS TO(_BASE)
+
+#define OSL_NUM OSL(_NUM)
+#define SET_NUM TO(_NUM)
+
+#define OSL_SYM OSL(_SYM)
+#define SET_SYM TO(_SYM)
+
+#define SPC_NAV LT(_NAV, KC_SPC)
+#define SET_NAV TO(_NAV)
+
+#define OSL_FN OSL(_FN)
+#define SET_FN TO(_FN)
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case DEL_HOM:
+      if (record->event.pressed) {
+        tap_code16(S(KC_HOME));
+        register_code(KC_BSPC);
+      } else {
+        unregister_code(KC_BSPC);
+      }
+
+      return false;
+
+    case DEL_END:
+      if (record->event.pressed) {
+        tap_code16(S(KC_END));
+        register_code(KC_DEL);
+      } else {
+        unregister_code(KC_DEL);
+      }
+
+      return false;
+  }
+
+  return true;
+};

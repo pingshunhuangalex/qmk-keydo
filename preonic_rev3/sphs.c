7a6449e39ba27960878e5546eb97c7b40659a180
Pingshun Huang <alex.g.huang@gmail.com>
2021-05-06 21:12:50 +1000

    Merge shift modifier with backspace
    Signed-off-by: Pingshun Huang <alex.g.huang@gmail.com>

--- preonic_rev3/sphs.c
+++ preonic_rev3/sphs.c
@@ -12,7 +12,7 @@ enum custom_keycodes {
 };
 
 // Layer Switchers
-#define OSM_SFT OSM(MOD_LSFT)
+#define BS_LSFT LSFT_T(KC_BSPC)
 #define SET_BAS TO(_BASE)
 
 #define OSL_NUM OSL(_NUM)
@@ -27,50 +27,15 @@ enum custom_keycodes {
 #define OSL_FN OSL(_FN)
 #define SET_FN TO(_FN)
 
-// Home Row Mods
-// #define HGUI_A LGUI_T(KC_A)
-// #define HALT_R LALT_T(KC_R)
-// #define HSFT_S LSFT_T(KC_S)
-// #define HCTL_T LCTL_T(KC_T)
+bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case BS_LSFT:
+            return true;
 
-// #define HCTL_N RCTL_T(KC_N)
-// #define HSFT_E RSFT_T(KC_E)
-// #define HALT_I RALT_T(KC_I)
-// #define HGUI_O RGUI_T(KC_O)
-
-// uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
-//     switch (keycode) {
-//         case HGUI_A:
-//         case HALT_R:
-//         case HSFT_S:
-//         case HCTL_T:
-//         case HCTL_N:
-//         case HSFT_E:
-//         case HALT_I:
-//         case HGUI_O:
-//             return TAPPING_TERM + 50;
-
-//         default:
-//             return TAPPING_TERM;
-//     }
-// }
-
-// bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
-//     switch (keycode) {
-//         case HGUI_A:
-//         case HALT_R:
-//         case HSFT_S:
-//         case HCTL_T:
-//         case HCTL_N:
-//         case HSFT_E:
-//         case HALT_I:
-//         case HGUI_O:
-//             return true;
-
-//         default:
-//             return false;
-//     }
-// }
+        default:
+            return false;
+    }
+}
 
 // Macros
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {

472ae4f85453254faae6952462617535f998d1e7
Pingshun Huang <alex.g.huang@gmail.com>
2021-06-18 23:10:15 +1000

    Unify keycode naming
    Signed-off-by: Pingshun Huang <alex.g.huang@gmail.com>

--- preonic_rev3/sphs.c
+++ preonic_rev3/sphs.c
@@ -1,38 +1,5 @@
-enum preonic_layers {
-    _BASE = 0,
-    _NUM,
-    _SYM,
-    _MK,
-    _NAV,
-    _FN
-};
-
-enum custom_keycodes {
-    DEL_HOM = SAFE_RANGE,
-    DEL_END
-};
-
-// Modifier Mods
 #define OSM_SFT OSM(MOD_LSFT)
 #define BSL_GUI GUI_T(KC_BSLS)
-
-// Layer Switchers
-#define SET_BAS TO(_BASE)
-
-#define OSL_NUM OSL(_NUM)
-#define SET_NUM TO(_NUM)
-
-#define OSL_SYM OSL(_SYM)
-#define SET_SYM TO(_SYM)
-
-#define BSPC_MK LT(_MK, KC_BSPC)
-#define SET_MK TO(_MK)
-
-#define SPC_NAV LT(_NAV, KC_SPC)
-#define SET_NAV TO(_NAV)
-
-#define ENT_FN LT(_FN, KC_ENT)
-#define SET_FN TO(_FN)
 
 bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
@@ -54,10 +21,9 @@ bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
     }
 };
 
-// Macros
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case DEL_HOM:
+        case DEL_LSW:
             if (record->event.pressed) {
                 tap_code16(S(KC_HOME));
                 register_code(KC_BSPC);
@@ -67,7 +33,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
             return false;
 
-        case DEL_END:
+        case DEL_LEW:
             if (record->event.pressed) {
                 tap_code16(S(KC_END));
                 register_code(KC_DEL);
---
+++ preonic_rev3/sphs_win.c
@@ -0,0 +1,30 @@
+enum preonic_layers_win {
+    _BSW = 0,
+    _NMW,
+    _SMW,
+    _MKW,
+    _NVW,
+    _FNW
+};
+
+enum preonic_keycodes_win {
+    DEL_LSW = SAFE_RANGE,
+    DEL_LEW
+};
+
+#define SET_BSW TO(_BSW)
+
+#define OSL_NMW OSL(_NMW)
+#define SET_NMW TO(_NMW)
+
+#define OSL_SMW OSL(_SMW)
+#define SET_SMW TO(_SMW)
+
+#define BSP_MKW LT(_MKW, KC_BSPC)
+#define SET_MKW TO(_MKW)
+
+#define SPC_NVW LT(_NVW, KC_SPC)
+#define SET_NVW TO(_NVW)
+
+#define ENT_FNW LT(_FNW, KC_ENT)
+#define SET_FNW TO(_FNW)
